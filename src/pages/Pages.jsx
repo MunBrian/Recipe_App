9ec988427584e9585d3f96a1d01d5c1aef3b35f7
Brian Mungai <bryanmungai254@gmail.com>
2022-09-28 15:12:14 +0300

    added category file with navlink to cuisine page. Configured fetch req with respect to params in link

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,11 +1,15 @@
+import { Route, Routes } from "react-router-dom"
 import Home from "./Home"
+import Cuisine from './Cuisine'
 
 const Pages = () => {
   return (
-      <div>
-          <Home />
-    </div>
+    <Routes>
+      <Route path='/' exact element={<Home />} />
+      <Route path='/cuisine/:type' element={<Cuisine />} />
+    </Routes>
   )
 }
+
 
 export default Pages
