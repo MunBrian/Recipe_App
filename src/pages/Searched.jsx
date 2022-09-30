3586b3309e1179f7f199356064ced96eeddf6ef0
Brian Mungai <bryanmungai254@gmail.com>
2022-09-30 15:01:18 +0300

    added searched pages to display searched recipes

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import styled from 'styled-components'
 
 const Searched = () => {
+  const [searchedRecipes, setSearchedRecipes] = useState([])
+  
+  let params = useParams()
+
+  useEffect(() => {
+    getSearched(params.search);
+  }, [params.search])
+
+
+  const getSearched = async (name) => {
+    try {
+      const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_RECIPE_API_KEY}&query=${name}`)
+      const recipes = await data.json()
+      setSearchedRecipes(recipes.results)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
   return (
-    <div>Searched</div>
+    <Grid>
+      {searchedRecipes.map(item => {
+        return (    
+            <Card key={item.id}>
+              <img src={item.image} alt="" />
+              <h4>{ item.title}</h4>
+            </Card>
+        )
+      })}
+    </Grid>
   )
 }
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+    grid-gap: 3rem;
+`
+
+const Card = styled.div`
+    img{
+       width: 100%;
+       border-radius :2rem ;
+    }
+
+    a{
+        text-decoration: none;
+    }
+    h4{
+        text-align: center;
+        padding: 1rem
+    }
+`
 
 export default Searched
